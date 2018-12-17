92c98455c42fe2d4d859b99c7bc49e1e9c2e7683
mfrachet <marvin.frachet@zenika.com>
2018-12-17 15:22:57 +0100

    adding disqus

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,16 +1,24 @@
 import React from 'react'
 import { Link, graphql } from 'gatsby'
+import { DiscussionEmbed } from 'disqus-react'
 
 import Bio from '../components/bio'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import { rhythm, scale } from '../utils/typography'
 
+const disqusShortname = 'acodingdance-io'
+
 class BlogPostTemplate extends React.Component {
   render() {
     const post = this.props.data.markdownRemark
     const siteTitle = this.props.data.site.siteMetadata.title
     const { previous, next } = this.props.pageContext
+
+    const disqusConfig = {
+      identifier: post.id,
+      title: post.frontmatter.title,
+    }
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
@@ -58,6 +66,8 @@ class BlogPostTemplate extends React.Component {
             )}
           </li>
         </ul>
+
+        <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
       </Layout>
     )
   }
