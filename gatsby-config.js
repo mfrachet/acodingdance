3df41012f1265539aa99a63e140cfa4b95e3c418
Marvin Frachet <marvin.frachet@gmail.com>
2019-03-23 18:03:25 -0400

    adding analytics (#12)

--- gatsby-config.js
+++ gatsby-config.js
@@ -83,5 +83,19 @@ module.exports = {
         pathToConfigModule: 'src/utils/typography',
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-136744956-1',
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ['/preview/**'],
+      },
+    },
   ],
 }
