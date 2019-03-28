70cd5a1e2a09634f1320f60733470e2d0a7f0060
Marvin Frachet <marvin.frachet@gmail.com>
2019-03-28 14:26:10 -0400

    Fixing analytics (#14)

--- gatsby-config.js
+++ gatsby-config.js
@@ -86,15 +86,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: 'UA-136744956-1',
-        // Puts tracking script in the head instead of the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ['/preview/**'],
+        trackingId: 'UA-136744956-1'
       },
     },
   ],
