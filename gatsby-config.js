8b6168ffd8c01e7acf1fd4f30bd70146ea6a6122
Marvin Frachet <marvin.frachet@bnc.ca>
2019-05-15 08:14:13 -0400

    fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,7 +68,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `/static/assets/fav.png`,
+        icon: `static/assets/fav.png`,
       },
     },
     `gatsby-plugin-offline`,
