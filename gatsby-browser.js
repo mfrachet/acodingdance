ba48f21f00ba5b671a867903346e8f649b039de3
Marvin Frachet <marvin.frachet@gmail.com>
2019-10-15 17:03:02 -0400

    Removing disqus (#38)

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,5 @@
 // custom typefaces
 import 'typeface-montserrat'
+import 'prismjs/themes/prism-solarizedlight.css'
 import 'typeface-roboto'
 import 'antd/dist/antd.css'
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../index.css'
 import { Container } from './container'
 import { Wrapper } from './wrapper'
 import { Layout as AntLayout } from 'antd'
