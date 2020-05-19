f9be5dbd0b3ef0660a68c633e90d09e3b726f2f7
mfrachet <marvin@gatsbyjs.com>
2020-05-19 13:16:45 +0200

    First rewrite

---
+++ gatsby-config.js
@@ -0,0 +1,71 @@
+require(`dotenv`).config({
+  path: `.env`,
+});
+
+module.exports = {
+  siteMetadata: {
+    siteTitle: "Just a blog",
+    siteTitleAlt: `Just a blog`,
+  },
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-minimal-blog`,
+      // See the theme's README for all available options
+      options: {
+        navigation: [
+          {
+            title: `Blog`,
+            slug: `/blog`,
+          },
+          {
+            title: `About`,
+            slug: `/about`,
+          },
+        ],
+        externalLinks: [
+          {
+            name: `Twitter`,
+            url: `https://twitter.com/mfrachet`,
+          },
+          {
+            name: `Github`,
+            url: `https://github.com/mfrachet`,
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+      },
+    },
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Just a blog`,
+        short_name: `my-journey`,
+        description: `Talking of my daily work and job`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#6B46C1`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify`,
+  ],
+};
