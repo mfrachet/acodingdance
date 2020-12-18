9bc6c125d7b675108301ee4bac018c697c26ab50
mfrachet <marvin@gatsbyjs.com>
2020-12-18 10:08:39 +0100

    Updates

--- sw.js
+++ sw.js
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "4d8e4361c0d2eb90aa418ffe653be3d6"
+    "revision": "4372b5519cc932c2869c3eb6abec8fed"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-ad6431e4664bcf916d19.js"
