348813b83026b44e8b21d287e70fd52b647542ac
Marvin Frachet <marvin.frachet@gmail.com>
2020-12-18 10:20:15 +0100

    Robotxt (#48)

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,31 +1,36 @@
 module.exports = {
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-emotion`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `markdown-pages`,
-        path: `${__dirname}/src/posts`,
-      },
+    siteMetadata: {
+        siteUrl: 'https://mfrachet.github.io',
     },
-
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-prismjs`,
-          {
-            resolve: `gatsby-remark-images`,
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        `gatsby-plugin-emotion`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-source-filesystem`,
             options: {
-              maxWidth: 720,
+                name: `markdown-pages`,
+                path: `${__dirname}/src/posts`,
             },
-          },
-        ],
-      },
-    },
-    `gatsby-plugin-offline`,
-  ],
+        },
+
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                plugins: [
+                    `gatsby-remark-copy-linked-files`,
+                    `gatsby-remark-prismjs`,
+                    {
+                        resolve: `gatsby-remark-images`,
+                        options: {
+                            maxWidth: 720,
+                        },
+                    },
+                ],
+            },
+        },
+        `gatsby-plugin-offline`,
+        `gatsby-plugin-sitemap`,
+        'gatsby-plugin-robots-txt',
+    ],
 };
